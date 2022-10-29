cef5eb4e787b22161c2c4fe3d058757dc15569c8
221Roberto221 <andrade.roberto@uabc.edu.mx>
2022-10-28 18:30:36 -0700

    Agregar archivos de proyecto.

---
+++ LoginBaseDatos/LoginBaseDatos/Views/LoginPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using LoginBaseDatos.Tables;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginBaseDatos.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Handle_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegistrationPage());
+        }
+
+        private void Handle_Clicked_1(object sender, EventArgs e)
+        {
+            var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserDataBase.db");
+            var db = new SQLiteConnection(dbpath);
+            var myquery = db.Table<RegUserTable>().Where(u=>u.UserName.Equals(EntryUser.Text) && u.Password.Equals(EntryPassword.Text)).FirstOrDefault();
+
+            if (myquery != null)
+            {
+                App.Current.MainPage = new NavigationPage(new);
+            }
+
+        }
+    }
+}
